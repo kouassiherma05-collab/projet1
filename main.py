cdb3ab40c61c135b115a681c57f4eceab1a90d04
kouassi jean hermann kouakou <kouassiherma05@gmail.com>
2026-04-14 21:43:07 +0100

    v1

---
+++ main.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+conn = sqlite3.connect("ma_base.db")
+conn.close()
+
+print("Base créée !")
+
+
+
+
---
+++ script.js
@@ -0,0 +1,7 @@
+const info = document.querySelector("info")
+
+const allbtns = document.querySelectorAll('input[type="touches"]') 
+
+function val(resutat){
+    form.fenetre.onclick =  form.fenetre.onclick + resutat
+}
