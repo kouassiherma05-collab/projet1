679069075793b3c3b685cf92309ce521671e77a7
unknown <lezouange0@gmail.com>
2026-04-16 01:40:38 +0000

    nouvel

--- script.js
+++ script.js
@@ -8,6 +8,53 @@ function afficher(nombre)
 }
 
 function appuyerchiffres(chiffre){
-    
+    nbractuel += chiffre;
+    afficher(nbractuel);    
 
+}
+
+function appuyeroperateur(op){
+    nbrprecedent = nbractuel;
+    nbractuel = "";
+    operateur = op;
+    afficher("0");
+}
+function calculer(){
+    
+    var num1 = parseFloat(nbrprecedent);
+    var num2 = parseFloat(nbractuel);
+    var resultat = 0;
+
+
+
+    if (operateur === "+"){
+        resultat = num1 + num2;
+    }
+    else if (operateur === "-"){
+        resultat = num1 - num2;
+    }
+    else if (operateur === "*"){
+        resultat = num1 * num2;
+    }
+    else if (operateur === "/"){
+        resultat = num1 / num2;
+    }
+    afficher(resultat);
+    nbractuel = resultat.toString();
+    nbrprecedent = "";
+    operateur = "";
+}
+
+
+function effacerone(){
+    nbractuel = nbractuel.slice(0, -1);
+    afficher(nbractuel);
+}
+
+
+function effacertout(){
+    nbractuel = "";
+    nbrprecedent = "";
+    operateur = "";
+    afficher("0");
 }
